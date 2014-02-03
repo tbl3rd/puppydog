b877b2f82ebe45120227bf56a415c038d534fd4c
Tom Lyons <tbl3rd@gmail.com>
2014-02-03 16:49:17 -0500

    Checkpoint FernsClassifier constructor.

--- source/ferns.cpp
+++ source/ferns.cpp
@@ -10,8 +10,8 @@ struct FernsClassifierParameters
     int fernCount;
     int featureCount;
     float fernValid;
-    float confidenceLevel;
-    float fernThrehsold;
+    float correlationThreshold;
+    float fernThreshold;
     float nearestNeighborThreshold;
     float nearestNeighborValid;
 
@@ -24,8 +24,8 @@ struct FernsClassifierParameters
         FCPIN(fn, p, fernCount);
         FCPIN(fn, p, featureCount);
         FCPIN(fn, p, fernValid);
-        FCPIN(fn, p, confidenceLevel);
-        FCPIN(fn, p, fernThrehsold);
+        FCPIN(fn, p, correlationThreshold);
+        FCPIN(fn, p, fernThreshold);
         FCPIN(fn, p, nearestNeighborThreshold);
         FCPIN(fn, p, nearestNeighborValid);
         return fs;
@@ -40,8 +40,8 @@ struct FernsClassifierParameters
         FCPOUT(fs, p, fernCount);
         FCPOUT(fs, p, featureCount);
         FCPOUT(fs, p, fernValid);
-        FCPOUT(fs, p, confidenceLevel);
-        FCPOUT(fs, p, fernThrehsold);
+        FCPOUT(fs, p, correlationThreshold);
+        FCPOUT(fs, p, fernThreshold);
         FCPOUT(fs, p, nearestNeighborThreshold);
         FCPOUT(fs, p, nearestNeighborValid);
         fs << "}";
@@ -49,16 +49,17 @@ struct FernsClassifierParameters
     }
 
     FernsClassifierParameters(const char *fileName)
-        : fernCount(0),
-          featureCount(0),
-          fernValid(0.0),
-          confidenceLevel(0.0),
-          fernThrehsold(0.0),
-          nearestNeighborThreshold(0.0),
-          nearestNeighborValid(0.0)
+        : fernCount(0)
+        , featureCount(0)
+        , fernValid(0.0)
+        , correlationThreshold(0.0)
+        , fernThreshold(0.0)
+        , nearestNeighborThreshold(0.0)
+        , nearestNeighborValid(0.0)
     {
-        const cv::FileStorage fs(fileName, cv::FileStorage::READ);
+        cv::FileStorage fs(fileName, cv::FileStorage::READ);
         if (fs.isOpened()) fs >> *this;
+        fs.release();
     }
 };
 #undef  FCPOUT
@@ -70,8 +71,8 @@ struct FernsClassifierParameters
 //
 class FernsClassifierFeature
 {
-    const cv::Point_<uchar> p;
-    const cv::Point_<uchar> q;
+    cv::Point_<uchar> p;
+    cv::Point_<uchar> q;
 public:
     FernsClassifierFeature()
         : p(0, 0), q(0, 0)
@@ -93,33 +94,66 @@ class FernsClassifier {
     typedef FernsClassifierFeature Feature;
     typedef std::vector<Feature> Fern;
 
-    Parameters itsParameters;
+    const Parameters itsParameters;
+
+    // Threshold for a negative match.
+    //
+    const float itsNegativeThreshold;
+
+    // Threshold for a positive match.
+    //
+    const float itsPositiveThreshold;
 
     // This classifier has itsParameters.fernCount ferns.
     // Each fern has itsParameters.featureCount features.
     //
     std::vector<Fern> itsFerns;
 
-    // Threshold for a negative match, initially:
-    // 0.5 * itsParameters.fernCount
-    //
-    float itsNegativeThreshold;
-
-    // Threshold for a positive match, initially:
-    // itsParameters.fernThreshold * itsParameters.fernCount
-    //
-    float itsPositiveThreshold;
-
+    std::vector< std::vector<float> > itsPosteriors;
     std::vector< std::vector<int> > itsNegativeCounts;
     std::vector< std::vector<int> > itsPositiveCounts;
-    std::vector< std::vector<float> > itsPosteriors;
 
     std::vector<cv::Mat> itsNegativePatches;
     std::vector<cv::Mat> itsPositivePatches;
 
 public:
 
-    FernsClassifier(const char *xml)
+    FernsClassifier(const char *xml, const std::vector<cv::Size> &scales)
         : itsParameters(xml)
-    {}
+        , itsNegativeThreshold(0.5 * itsParameters.fernCount)
+        , itsPositiveThreshold(itsParameters.fernThreshold
+                               * itsParameters.fernCount)
+        , itsFerns(0)
+        , itsPosteriors(0)
+        , itsNegativeCounts(0)
+        , itsPositiveCounts(0)
+        , itsNegativePatches(0)
+        , itsPositivePatches(0)
+    {
+        static cv::RNG rng;
+        const int fernCount = itsParameters.fernCount;
+        const int featureCount = itsParameters.featureCount;
+        const int totalFeatures = fernCount * featureCount;
+        const std::size_t scalesCount = scales.size();
+        itsFerns = std::vector<Fern>(scalesCount, Fern(totalFeatures));
+        for (int i = 0; i < totalFeatures; ++i) {
+            const cv::Point_<float> p1((float)rng, (float)rng);
+            const cv::Point_<float> p2((float)rng, (float)rng);
+            for (std::size_t j = 0; j < scalesCount; ++j) {
+                Fern &f = itsFerns[j];
+                const cv::Size &s = scales[j];
+                const cv::Point_<uchar> p(p1.x * s.width, p1.y * s.height);
+                const cv::Point_<uchar> q(p2.x * s.width, p2.y * s.height);
+                f[i] = Feature(p, q);
+            }
+        }
+        for (int i = 0; i < fernCount; ++i) {
+            const std::size_t count = 1 << featureCount;
+            const std::vector<float> floatZeros(count, 0.0);
+            const std::vector<int> intZeros(count, 0);
+            itsPosteriors.push_back(floatZeros);
+            itsNegativeCounts.push_back(intZeros);
+            itsPositiveCounts.push_back(intZeros);
+        }
+    }
 };
